81269c72342fdbea9fbcae138d6cf1082be3571e
ArthurBispo00 <arthurbispolima@hotmail.com>
2025-09-24 20:20:17 -0300

    Alterações no backend

--- backend/app.py
+++ backend/app.py
@@ -87,4 +87,4 @@ def tryon():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000) # É bom especificar a porta
+    app.run(debug=True, port=8000) # É bom especificar a porta
